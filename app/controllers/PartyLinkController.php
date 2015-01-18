70820bbcc10f8f3c5b4624f524f50f818022c722
gbrock <contact@gbrock.com>
2015-01-18 00:56:43 -0500

    Built initial controllers

---
+++ app/controllers/PartyLinkController.php
@@ -0,0 +1,86 @@
+<?php
+
+class PartyLinkController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index($partyId)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create($partyId)
+	{
+		//
+	}
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store($partyId)
+	{
+		//
+	}
+
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($partyId, $id)
+	{
+		//
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($partyId, $id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($partyId, $id)
+	{
+		//
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($partyId, $id)
+	{
+		//
+	}
+
+
+}
--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,6 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', 'PartyController@index');
+Route::resource('party', 'PartyController');
+Route::resource('party.link', 'PartyLinkController');
