831bc349eca8d554c74c13021a6e7d500d618473
gbrock <contact@gbrock.com>
2015-01-19 02:46:30 -0500

    Another straggler...?

---
+++ app/controllers/PartyAddressController.php
@@ -0,0 +1,11 @@
+<?php
+
+class PartyAddressController extends \PartyLocatorController {
+
+	protected $data_key = 'address';
+	protected $data_key_p = 'addresses';
+	protected $breadcrumb_name = 'address';
+	protected $view_folder = 'party_addresses';
+	protected $model_name = 'PartyAddress';
+	
+}
