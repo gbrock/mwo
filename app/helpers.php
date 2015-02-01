8de053c6c2f81519b3edc9ddf18dd0dd032bb74f
gbrock <contact@gbrock.com>
2015-02-01 00:23:16 -0500

    Added helper function pairify()
    Makes a simple key-value pair based on an array of objects (usually
    database records).

--- app/helpers.php
+++ app/helpers.php
@@ -46,3 +46,50 @@ if(!function_exists('dd'))
 		}
 	}
 }
+
+if(!function_exists('pairify'))
+{
+	/**
+	 * Make a simple key-value pair based on an array of objects, usually
+	 * database records.
+	 * @param  array $oVar  The array of objects
+	 * @param  string $key   Which object key should be used as the new Key
+	 * @param  string $value Which object key should be used as the new Value
+	 * @return array        The simplified array
+	 */
+	function pairify($oVar, $key, $value)
+	{
+		/**
+		 * The return array.
+		 * @var array
+		 */
+		$r = array();
+
+		if(!is_array($oVar))
+		{
+			throw new Exception('pairify() requires an array of objects.');
+		}
+
+		if(!is_string($key) || !is_string($value))
+		{
+			throw new Exception('pairify() requires strings to set as "key" and "value".');
+		}
+
+		// If the set isn't empty
+		if(count($oVar) > 0)
+		{
+			foreach($oVar as $record)
+			{
+				$record_k = isset($record->{$key}) ? strval($record->{$key}) : NULL;
+				$record_v = isset($record->{$value}) ? $record->{$value} : NULL;
+
+				if($record_k)
+				{
+					$r[$record_k] = $record_v;
+				}
+			}
+		}
+
+		return $r;
+	}
+}
